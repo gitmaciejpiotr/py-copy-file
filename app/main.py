e4dfafd546017a1fa1bc1ab9ee44463eef1a5873
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-02-02 19:16:34 +0100

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,6 @@
 # write your code here
+def copy_file(command: str, filename: str, copy_name: str) -> None:
+    if command == "cd" and filename != copy_name:
+        with open(filename, "r") as file_in, open(copy_name, "w") as file_out:
+            original_text = file_in.read()
+            file_out.write(original_text)
